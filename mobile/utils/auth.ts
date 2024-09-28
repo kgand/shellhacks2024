70ca026e5f47d64c9a83201ccd5cf058f26765a1
kgand <34491606+kgand@users.noreply.github.com>
2024-09-28 16:08:52 -0400

    updated menu search and api calls to backend

--- mobile/utils/auth.ts
+++ mobile/utils/auth.ts
@@ -5,14 +5,27 @@ export const loginWithEmailAndPassword = async (email: string, password: string)
     try {
         const response = await signInWithEmailAndPassword(auth, email, password);
         return { user: auth.currentUser };
-      } catch (e) {
+    } catch (e) {
         return { error: e };
-      }
+    }
 }
 
 export const registerWithEmailandPassword = async (email: string, password: string) => {
     try {
         const response = await createUserWithEmailAndPassword(auth, email, password);
+        const userId = auth.currentUser?.uid;
+
+        // Send a request to your MongoDB endpoint to create a user
+        await fetch('http://10.108.74.57:5000/api/create_user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userID: userId // Ensure the key is userID as expected by the backend
+            }),
+        });
+
         return { user: auth.currentUser };
     } catch (e) {
         return { error: e };
