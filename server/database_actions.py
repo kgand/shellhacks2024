c258dd5135795ea0f492a43c6c336be122bc75b0
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2024-09-28 04:54:55 -0400

    Database actions

---
+++ server/database_actions.py
@@ -0,0 +1,49 @@
+import firebase_admin
+from firebase_admin import db, firestore
+
+cred_obj = firebase_admin.credentials.Certificate('noted-ad0e9-firebase-adminsdk-2q5p8-fe74a4b463.json')
+databaseURL = "https://noteddb.firebaseio.com"
+default_app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL':databaseURL
+    })
+
+db = firestore.client()
+
+def get_classes(userID: str):
+    # Reference to the users node
+    users_ref = db.collection(userID)  # Adjust this name based on your Firestore structure
+    
+    # Get all users
+    docs = users_ref.stream()
+
+    classes_array = []
+
+    if docs:
+        for doc in docs:
+            classes_array = list(doc.to_dict().keys())
+            print(f"User ID: {doc.id}, Info: {doc.to_dict().keys()}")
+    else:
+        print("None")
+    return classes_array
+
+def upload_note(userID: str, class_name: str, img_data: str, topics: list):
+    # Reference to the users collection
+    users_ref = db.collection(f"{userID}/{class_name}/RzreNkONSIOOF7uoEBl3")  # Adjust this name if needed
+
+    new_note = {
+        "image": img_data
+    }
+
+    for n in topics:
+        new_note[n] = 0
+
+    users_ref.set(new_note)
+
+    # Set the user data (this will create a new document or overwrite an existing one)
+    users_ref.push().set(new_note)
+
+    print(f"User data uploaded for User ID: {userID}")
+
+
+
+upload_note("user1", "class1", "14q313q23131", ["Greens THeorem", "INtegration"] )   
